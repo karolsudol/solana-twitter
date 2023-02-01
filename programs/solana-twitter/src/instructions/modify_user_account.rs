a39090d3252ee28cc94114110cabec62f3148043
elcomendante <karol@finsane.com>
2023-02-01 09:20:51 +0000

    modify

--- programs/solana-twitter/src/instructions/modify_user_account.rs
+++ programs/solana-twitter/src/instructions/modify_user_account.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-use crate::create_user_account::SolanaTwitterAccountInfo;
+use crate::{create_user_account::SolanaTwitterAccountInfo};
 
 pub fn modify_user_account(
     ctx: Context<ModifyUserAccount>,
@@ -10,14 +10,35 @@ pub fn modify_user_account(
 ) -> Result<()> {
     msg!("Modifying Solana Twitter account...");
 
-    // TODO: Modify Twitter Account
+    let existing_twitter_account = &mut ctx.accounts.twitter_account.key();
+    existing_twitter_account.handle = handle;
+    existing_twitter_account.display_name = display_name;
 
     msg!("Solana Twitter account updated successfully.");
     Ok(())
 }
 
 #[derive(Accounts)]
-
+#[instruction(
+    handle: String,
+    display_name: String,
+    twitter_account_bump: u8,
+)]
 pub struct ModifyUserAccount<'info> {
-    // TODO: Modify Twitter Account Context
+    #[account(
+        mut, 
+        seeds = [
+            authority.key().as_ref(),
+            b"_profile",
+
+        ],
+        bump = twitter_account_bump
+    )]
+
+    pub twitter_account: Account<'info, SolanaTwitterAccountInfo>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info,System>
 }
